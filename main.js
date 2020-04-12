e46170f212c14ef77510b34bf0b0c0d49844df59
anshusaurav <anshu.saurav@gmail.com>
2020-04-12 23:50:11 +0530

    'Added-JS-objects'

---
+++ main.js
@@ -0,0 +1,6 @@
+videoArr = [{id: 1, username: 'Cartoon Penguin', userimg: 'assets\media\image-1.png', videourl: 'assets/media/clip.mp4',
+ gamename: 'FireFly', parlorname: 'KopanVR', descone:'Had a blast yesterday at KopanVR and scores 87', desctwo: 'Can you beat it? hahaha Follow for more...'},
+            {},
+            {},
+            {},
+            {}];
