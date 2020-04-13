8c9af84de067e00b8aff207d5d89815af91f87bf
anshusaurav <anshu.saurav@gmail.com>
2020-04-13 19:21:57 +0530

    'First-Step-Towards-Dynamic-Content'

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-videoArr = [{id: 1, username: 'Cartoon Penguin', userimg: 'assets\media\image-1.png', videourl: 'assets/media/clip.mp4',
- gamename: 'FireFly', parlorname: 'KopanVR', descone:'Had a blast yesterday at KopanVR and scores 87', desctwo: 'Can you beat it? hahaha Follow for more...'},
-            {},
-            {},
-            {},
-            {}];
+videoArr = [
+            {id: 1, username: 'Cartoon Penguin', userimg: 'assets\media\image-1.png', videourl: 'assets/media/clip1.mp4', gamename: 'FireFly', parlorname: 'KopanVR', descone:'Had a blast yesterday at KopanVR and scores 87', desctwo: 'Can you beat it? hahaha Follow for more...'},
+            {id: 2, username: 'Little Penguin', userimg: 'assets\media\image-2.png', videourl: 'assets/media/clip2.mp4', gamename: 'Pavlov', parlorname: 'VivianVR', descone:'Had so much fun at the arcade', desctwo: 'Checkout game on steam. '},
+            {id: 3, username: 'Yellow-Eyed Penguin', userimg: 'assets\media\image-3.png', videourl: 'assets/media/clip3.mp4', gamename: 'Shadow-Legend', parlorname: 'Arena-Space', descone:'Had so much fun at the arcade', desctwo: 'Checkout game on steam. '},
+            {id: 4, username: 'Snares Penguin', userimg: 'assets\media\image-4.png', videourl: 'assets/media/clip4.mp4', gamename: 'Boneworks', parlorname: 'MK2 VR', descone:'Had so much fun at the arcade', desctwo: 'Checkout game on steam. '},
+            {id: 5, username: 'Emperor Penguin', userimg: 'assets\media\image-5.png', videourl: 'assets/media/clip5.mp4', gamename: 'Counter-Fight', parlorname: 'VivianVR', descone:'Had so much fun at the arcade', desctwo: 'Checkout game on steam. '},
+            ];
