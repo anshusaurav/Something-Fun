d47a582cb30e49daa68f861dab193c1a694fed08
anshusaurav <anshu.saurav@gmail.com>
2020-04-14 00:30:27 +0530

    'slider-work'

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ videoArr = [
             {id: 2, username: 'Little Penguin', userimg: 'assets/media/image-2.png', videourl: 'assets/media/clip-2.mp4', gamename: 'Pavlov', parlorname: 'VivianVR', descone:'Had so much fun at the arcade', desctwo: 'Checkout game on steam. '},
             {id: 3, username: 'Yellow-Eyed Penguin', userimg: 'assets/media/image-3.png', videourl: 'assets/media/clip-3.mp4', gamename: 'Shadow-Legend', parlorname: 'Arena-Space', descone:'Had so much fun playing Shadow Legend', desctwo: 'Checkout game on steam. '},
             {id: 4, username: 'Snares Penguin', userimg: 'assets/media/image-4.png', videourl: 'assets/media/clip-4.mp4', gamename: 'Boneworks', parlorname: 'MK2 VR', descone:'Had so much fun playing Shadow Legend at MK2 VR', desctwo: 'Checkout game on steam. '},
-            {id: 5, username: 'Emperor Penguin', userimg: 'assets/media/image-5.png', videourl: 'assets/media/clip-5.mp4', gamename: 'Counter-Fight', parlorname: 'VivianVR', descone:'Had so much fun playing at VivianVR', desctwo: 'Checkout game on steam. '},
+            {id: 5, username: 'Emperor Penguin', userimg: 'assets/media/image-5.png', videourl: 'assets/media/clip-5.mp4', gamename: 'Counter-Fight', parlorname: 'VivianVR', descone:'Had so much fun playing at VivianVR', desctwo: 'Checkout game on steam. '}
             ];
 //https://codepen.io/butlerx/details/xgGaWr
 
 let sliderIndex = 0;
+let preIn = 1;
+let postIn = videoArr.length-1;
 function plusDiv(shiftNum){
     sliderIndex += shiftNum;
     sliderIndex %= videoArr;
@@ -17,6 +19,8 @@ let leftElem = document.querySelector('.right-arrow');
 let introElem = document.querySelector('.intro-content');
 let descElem = document.querySelector('.desc-content');
 let videoElem = document.querySelector('.video-bg');
+let prevVidElem = document.querySelector('.video-prev');
+let nextVidElem = document.querySelector('.video-next');
 rightElem.addEventListener('click', next);
 leftElem.addEventListener('click', previous);
 
@@ -66,5 +70,14 @@ function loadNewSliders(sliderIndex){
     descElem = innerHTML = 
    `<p>${videoArr[sliderIndex].descone}</p>
     <p>${videoArr[sliderIndex].desctwo}</p>  `;
-    videoElem.setAttribute('src', videoArr[sliderIndex].videourl)
+    videoElem.setAttribute('src', videoArr[sliderIndex].videourl);
+    preIn = sliderIndex+1;
+    if(preIn < 0)
+        preIn = videoArr.length-1;
+    postIn = sliderIndex-1;
+    if(postIn >= videoArr.length)
+        postIn = 0;
+    console.log(preIn, postIn);
+    prevVidElem.setAttribute('src', videoArr[preIn].videourl );
+    nextVidElem.setAttribute('src', videoArr[postIn].videourl );
 }
